6961279eace0ee008c3b3a034015d8bee6e4b16b
Rimsha Naveed <rimshanaveed1786@yahoo.com>
2018-02-20 01:27:13 -0800

    send ti github

--- gull/Form1.Designer.cs
+++ gull/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(142, 80);
+            this.button1.Location = new System.Drawing.Point(142, 94);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
